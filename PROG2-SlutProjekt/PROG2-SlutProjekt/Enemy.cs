7e6d645d15ceac97acf51755e4a6892570680104
MathiasK <55237242+MatteKiwi@users.noreply.github.com>
2020-04-29 17:53:43 +0200

    comments

--- PROG2-SlutProjekt/PROG2-SlutProjekt/Enemy.cs
+++ PROG2-SlutProjekt/PROG2-SlutProjekt/Enemy.cs
@@ -8,21 +8,22 @@ namespace PROG2_SlutProjekt
 {
     class Enemy:Character
     { 
+        //en proprety som leder till inkappslingen av stringen name, som avnändes för att visa namnet på "enemy"
         public string Name { get; private set; }
-
+        //En int för enemys "livspoäng" som är satt på 150
         private int enemyHp = 150;
-      
+        //Konsturktor för enemy class, innehåller namnet på alla "enemies" som "spawnar"
         public Enemy()
         {
             Name = "Clas Göran ";
             GetHp();
         }
-
+        //En override på GetHp metoden ifrån Character, den byter ut det "hp:et" som den ärver från den klassen med enemyHp 
         public override int GetHp()
         {         
             return enemyHp;
         }
-
+        //En override på Hurt metoden ifrån Character.
         public override void Hurt(int amount)
         {         
             enemyHp -= amount;
@@ -32,7 +33,7 @@ namespace PROG2_SlutProjekt
                 enemyHp = 0;
             }
         }
-
+        //En override på Attack metoden, inget speciellt med den
         public override int Attack(int dmg)
         {
             return dmg;
--- PROG2-SlutProjekt/PROG2-SlutProjekt/Game.cs
+++ PROG2-SlutProjekt/PROG2-SlutProjekt/Game.cs
@@ -8,41 +8,44 @@ using System.Threading;
 namespace PROG2_SlutProjekt
 {
     class Game
-    {      
+    {    
+        //En konsturktor av Game som innehåller hela "spelet", dess syfte är att köra spelet.
         public Game()
         {
+            //skapar två instanter från klasserna Enemy samt Player
             Enemy E1 = new Enemy();
             Player P1 = new Player();
-
+            //Lägger in instanerna E1 samt P1 i en lista.
             CharacterList(E1, P1);
-
+            //Skapar en instans av klassen Story samt köra Welcome metoden
             Story story = new Story();
             story.Welcome();
-
+            //En bool för while loopen
             bool gameOver = false;
+            //While loop som innehåller det viktagaste av spelet.
             while (!gameOver)
             {
                 string input = Console.ReadLine();
 
                 if (input.ToLower() == "y")
                 {
-                    StoryTime(story);
+                    StoryTime(story);//kör storytime metoden som innehåller "början" av spelet.
                     int n = UserInput();
 
                     if (n == 2)
                     {
-                        Battle(story, E1, P1);
+                        Battle(story, E1, P1);//kör hela fight metoden där hela spelets "fight" finns i.
 
-                        gameOver = true;
+                        gameOver = true; 
                     }
 
-                    else if (n == 3)
+                    else if (n == 3) //avslutar spelet ifall spelaren försöker springa iväg.
                     {
                         story.Run();
                         gameOver = true;
                     }
 
-                    else if (n == 4)
+                    else if (n == 4) //Jag skulle istället ha velat haft en while loop men jag hann inte implantera en på ett effektivt sätt
                     {
                         Weapon W1 = new Weapon();
                         Console.WriteLine("Name: " + W1.Name + " type: " + W1.Type);
@@ -51,7 +54,7 @@ namespace PROG2_SlutProjekt
                     }
 
                 }
-                else if (input.ToLower() == "n")
+                else if (input.ToLower() == "n") //avslutar spelet ifall spelaren svarar nej på " the quest"
                 {
                     gameOver = true;
                 }
@@ -60,14 +63,14 @@ namespace PROG2_SlutProjekt
 
             Console.ReadLine();
         }
-
+        //En metod med en lista som innehåller E1 och P1
         private static void CharacterList(Enemy E1, Player P1)
         {
             List<Character> allCharacters = new List<Character>();
             allCharacters.Add(E1);
             allCharacters.Add(P1);
         }
-
+        //Metod som innehåller the "battle" i spelet.
         private static void Battle(Story story, Enemy E1, Player P1)
         {
             Console.Clear();
@@ -100,7 +103,7 @@ namespace PROG2_SlutProjekt
             story.Death();
             Thread.Sleep(1000);
         }
-
+        //nåt försök på generisk klass
         private static void CharacterListTest()
         {
             /*nåt försök på polymorfism tror jag?
@@ -113,7 +116,7 @@ namespace PROG2_SlutProjekt
             }
             */
         }
-
+        //En metod med "Storyn" som presenteras i början av spelet
         private static void StoryTime(Story story)
         {
             Console.Clear();
@@ -121,7 +124,7 @@ namespace PROG2_SlutProjekt
             Console.ReadLine();
             story.Options();
         }
-
+        //En metod som innehåller en simpel Userinput som använder sig av while loop och tryparse
         private static int UserInput()
         {
             int n = 0;
